02c485cb079961948fa40e581fdd41f517b3763b
naldo <jhpjhp0223@gmail.com>
2016-08-25 17:54:15 +0900

    builtin Object - @function / @construct / getPrototypeOf

--- src/main/scala/kr/ac/kaist/safe/analyzer/models/builtin/BuiltinObject.scala
+++ src/main/scala/kr/ac/kaist/safe/analyzer/models/builtin/BuiltinObject.scala
@@ -11,16 +11,112 @@
 
 package kr.ac.kaist.safe.analyzer.models.builtin
 
+import kr.ac.kaist.safe.analyzer.{ Semantics, Helper }
+import kr.ac.kaist.safe.analyzer.domain._
 import kr.ac.kaist.safe.analyzer.models._
+import kr.ac.kaist.safe.util.{ SystemAddr, Loc, Recent }
 
-// TODO Object
+// 15.2 Object Objects
 object BuiltinObject extends FuncModel(
   name = "Object",
+
+  // 15.2.1 The Object Constructor Called as a Function: Object([value])
+  code = BasicCode(argLen = 1, (
+    args: Value, st: State, sem: Semantics, utils: Utils
+  ) => {
+    val h = st.heap
+    val argV = sem.CFGLoadHelper(args, Set(utils.absString.alpha("0")), h)
+    val addr = SystemAddr("<>instance<>Object")
+
+    // 1. If value is null, undefined or not supplied, create and return
+    //    a new Object object exactly as if the standard built-in Object
+    //    constructor had been called with the same arguments.
+    val pv = argV.pvalue
+    val (v1, st1) = if (pv.undefval.gamma != ConSimpleBot ||
+      pv.nullval.gamma != ConSimpleBot ||
+      argV.isBottom) {
+      val state = st.oldify(addr)(utils)
+      val loc = Loc(addr, Recent)
+      val obj = Obj.newObject(utils)
+      val heap = state.heap.update(loc, obj)
+      val ctx = state.context
+      (Value(loc)(utils), State(heap, ctx))
+    } else {
+      (Value.Bot(utils), State.Bot)
+    }
+
+    // 2. Return ToObject(value)
+    val (v2, st2, _) = Helper(utils).toObject(st, argV, addr)
+
+    (st1 + st2, State.Bot, v1 + v2)
+  }),
+
+  // 15.2.2 The Object Constructor: new Object([value])
+  construct = Some(BasicCode(argLen = 1, (
+    args: Value, st: State, sem: Semantics, utils: Utils
+  ) => {
+    val h = st.heap
+    val argV = sem.CFGLoadHelper(args, Set(utils.absString.alpha("0")), h)
+    val addr = SystemAddr("<>instance<>Object")
+
+    // 1. If value is supplied, then
+    //    a. If Type(value) is Object, then
+    //       i. If the value is a native ECMAScript object, do not create a new object
+    //          but simply return value.
+    //       ii. If the value is a host object, then actions are taken and a result is
+    //           returned in an implementation-dependent manner that may depend on
+    //           the host object.
+    //           (We do not consider an implementation-dependent actions for a host object)
+    //    b. If Type(value) is String, return ToObject(value).
+    //    c. If Type(value) is Boolean, return ToObject(value).
+    //    d. If Type(value) is Number, return ToObject(value).
+    val (v2, st2, _) = Helper(utils).toObject(st, argV, addr)
+
+    // 2. Assert: The argument value was not supplied or its type was Null or Undefined.
+    // 3. Let obj be a newly created native ECMAScript object.
+    val pv = argV.pvalue
+    val (v1, st1) = if (pv.undefval.gamma != ConSimpleBot ||
+      pv.nullval.gamma != ConSimpleBot ||
+      argV.isBottom) {
+      val state = st.oldify(addr)(utils)
+      val loc = Loc(addr, Recent)
+      val obj = Obj.newObject(utils)
+      val heap = state.heap.update(loc, obj)
+      val ctx = state.context
+      (Value(loc)(utils), State(heap, ctx))
+    } else {
+      (Value.Bot(utils), State.Bot)
+    }
+
+    (st1 + st2, State.Bot, v1 + v2)
+  })),
+
+  // 15.2.3.1 Object.prototype
+  protoModel = Some((BuiltinObjectProto, F, F, F)),
+
   props = List(
-    // TODO getPrototypeOf
+    // 15.2.3.2 Object.getPrototypeOf(O)
     ("getPrototypeOf", FuncModel(
       name = "Object.getPrototypeOf",
-      code = EmptyCode(argLen = 1)
+      code = BasicCode(argLen = 1, (
+        args: Value, st: State, sem: Semantics, utils: Utils
+      ) => {
+        val h = st.heap
+        val argV = sem.CFGLoadHelper(args, Set(utils.absString.alpha("0")), h)
+        val tmpAddr = SystemAddr("temp")
+
+        val (retV, retSt, excSet) = Helper(utils).toObject(st, argV, tmpAddr)
+
+        // 1. If Type(O) is not Object throw a TypeError exception.
+        val excSt = st.raiseException(excSet)(utils)
+
+        // 2. Return the value of [[Prototype]] internal property of O.
+        val protoV = retV.locset.foldLeft(Value.Bot(utils))((v, loc) => {
+          v + retSt.heap(loc).getOrElse(Obj.Bot(utils)).get("@proto")(utils).objval.value
+        })
+
+        (st, excSt, protoV)
+      })
     ), T, F, T),
 
     // TODO getOwnPropertyDescriptor
@@ -94,12 +190,7 @@ object BuiltinObject extends FuncModel(
       name = "Object.keys",
       code = EmptyCode(argLen = 1)
     ), T, F, T)
-  ),
-  // TODO @function
-  code = EmptyCode(argLen = 1),
-  // TODO @construct
-  construct = Some(EmptyCode()),
-  protoModel = Some((BuiltinObjectProto, F, F, F))
+  )
 )
 
 object BuiltinObjectProto extends ObjModel(
