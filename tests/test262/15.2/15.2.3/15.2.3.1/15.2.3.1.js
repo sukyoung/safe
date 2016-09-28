af7d06b85259336ce525da200696b14ca11dbdf1
Jihyeok Park <jhpjhp0223@gmail.com>
2016-09-29 02:07:25 +0900

    test262 for Object.prototype

---
+++ tests/test262/15.2/15.2.3/15.2.3.1/15.2.3.1.js
@@ -0,0 +1,3 @@
+var desc = Object.getOwnPropertyDescriptor(Object, 'prototype');
+var __result1 = desc.writable === false && desc.enumerable === false && desc.configurable === false;
+var __expect1 = true
