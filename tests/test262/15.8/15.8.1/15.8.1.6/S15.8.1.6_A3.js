5b916ba442ec00dfdf1512aaa304518353f53402
Yeonni <yeonhee1ryou@gmail.com>
2016-09-29 20:20:34 +0900

    test262: Math - 15.8.1.6

---
+++ tests/test262/15.8/15.8.1/15.8.1.6/S15.8.1.6_A3.js
@@ -0,0 +1,5 @@
+  {
+    var __result1 = delete Math.PI === true;
+    var __expect1 = false;
+  }
+  
---
+++ tests/test262/15.8/15.8.1/15.8.1.6/S15.8.1.6_A4.js
@@ -0,0 +1,7 @@
+  var x = Math.PI;
+  Math.PI = 1;
+  {
+    var __result1 = Math.PI !== x;
+    var __expect1 = false;
+  }
+  
