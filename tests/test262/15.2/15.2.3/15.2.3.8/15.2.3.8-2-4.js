1bc76617ca16e5d8432038ee5b685c9f869659d5
naldo <jhpjhp0223@gmail.com>
2016-10-01 15:52:58 +0900

    test262: automatically pass: 15.2.3.8

--- tests/test262/15.2/15.2.3/15.2.3.8/15.2.3.8-2-4.js
+++ tests/test262/15.2/15.2.3/15.2.3.8/15.2.3.8-2-4.js
@@ -1,22 +1,21 @@
-// TODO to Jihyeok
-// function testcase() 
-// {
-//   var obj = {
+function testcase() 
+{
+  var obj = {
 
-//   };
-//   Object.defineProperty(obj, "foo", {
-//     value : 10,
-//     enumerable : false,
-//     configurable : true
-//   });
-//   var preCheck = Object.isExtensible(obj);
-//   Object.seal(obj);
-//   var beforeDeleted = obj.hasOwnProperty("foo");
-//   delete obj.foo;
-//   var afterDeleted = obj.hasOwnProperty("foo");
-//   return preCheck && beforeDeleted && afterDeleted;
-// }
-// {
-//   var __result1 = testcase();
-//   var __expect1 = true;
-// }
+  };
+  Object.defineProperty(obj, "foo", {
+    value : 10,
+    enumerable : false,
+    configurable : true
+  });
+  var preCheck = Object.isExtensible(obj);
+  Object.seal(obj);
+  var beforeDeleted = obj.hasOwnProperty("foo");
+  delete obj.foo;
+  var afterDeleted = obj.hasOwnProperty("foo");
+  return preCheck && beforeDeleted && afterDeleted;
+}
+{
+  var __result1 = testcase();
+  var __expect1 = true;
+}
--- tests/test262/15.2/15.2.3/15.2.3.8/15.2.3.8-2-a-2.js
+++ tests/test262/15.2/15.2.3/15.2.3.8/15.2.3.8-2-a-2.js
@@ -1,25 +1,24 @@
-// TODO to Jihyeok
-// function testcase() 
-// {
-//   var proto = {
-//     foo : 0
-//   };
-//   var ConstructFun = (function () 
-//   {
+function testcase() 
+{
+  var proto = {
+    foo : 0
+  };
+  var ConstructFun = (function () 
+  {
 
-//   });
-//   ConstructFun.prototype = proto;
-//   var child = new ConstructFun();
-//   Object.defineProperty(child, "foo", {
-//     value : 10,
-//     configurable : true
-//   });
-//   var preCheck = Object.isExtensible(child);
-//   Object.seal(child);
-//   delete child.foo;
-//   return preCheck && child.foo === 10;
-// }
-// {
-//   var __result1 = testcase();
-//   var __expect1 = true;
-// }
+  });
+  ConstructFun.prototype = proto;
+  var child = new ConstructFun();
+  Object.defineProperty(child, "foo", {
+    value : 10,
+    configurable : true
+  });
+  var preCheck = Object.isExtensible(child);
+  Object.seal(child);
+  delete child.foo;
+  return preCheck && child.foo === 10;
+}
+{
+  var __result1 = testcase();
+  var __expect1 = true;
+}
