1feabe2034870df0b13004a302b99e16b3c89e6f
Jihyeok Park <jhpjhp0223@gmail.com>
2016-10-03 16:07:31 +0900

    fix error of delete in AbsMap / test262: delete comment for dataPropertyAttributesAreCorrect in 15.2.3.6

--- tests/test262/15.2/15.2.3/15.2.3.6/15.2.3.6-4-82-17.js
+++ tests/test262/15.2/15.2.3/15.2.3.6/15.2.3.6-4-82-17.js
@@ -1,23 +1,92 @@
-//   TODO rewrite dataPropertyAttributesAreCorrect
-//   function testcase() 
-//   {
-//     var obj = {
-//       
-//     };
-//     Object.defineProperty(obj, "0", {
-//       value : 1001,
-//       writable : true,
-//       enumerable : true,
-//       configurable : true
-//     });
-//     Object.defineProperty(obj, "0", {
-//       enumerable : false,
-//       configurable : false
-//     });
-//     return dataPropertyAttributesAreCorrect(obj, "0", 1001, true, false, false);
-//   }
-//   {
-//     var __result1 = testcase();
-//     var __expect1 = true;
-//   }
-//   
+  function testcase() 
+  {
+    var obj = {
+      
+    };
+    Object.defineProperty(obj, "0", {
+      value : 1001,
+      writable : true,
+      enumerable : true,
+      configurable : true
+    });
+    Object.defineProperty(obj, "0", {
+      enumerable : false,
+      configurable : false
+    });
+    return dataPropertyAttributesAreCorrect(obj, "0", 1001, true, false, false);
+  }
+  {
+    var __result1 = testcase();
+    var __expect1 = true;
+  }
+  
+function dataPropertyAttributesAreCorrect(obj,
+                                          name,
+                                          value,
+                                          writable,
+                                          enumerable,
+                                          configurable) {
+    var attributesCorrect = true;
+
+    if (obj[name] !== value) {
+        if (typeof obj[name] === "number" &&
+            isNaN(obj[name]) &&
+            typeof value === "number" &&
+            isNaN(value)) {
+            // keep empty
+        } else {
+            attributesCorrect = false;
+        }
+    }
+
+    try {
+        if (obj[name] === "oldValue") {
+            obj[name] = "newValue";
+        } else {
+            obj[name] = "OldValue";
+        }
+    } catch (we) {
+    }
+
+    var overwrited = false;
+    if (obj[name] !== value) {
+        if (typeof obj[name] === "number" &&
+            isNaN(obj[name]) &&
+            typeof value === "number" &&
+            isNaN(value)) {
+            // keep empty
+        } else {
+            overwrited = true;
+        }
+    }
+    if (overwrited !== writable) {
+        attributesCorrect = false;
+    }
+
+    var enumerated = false;
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop) && prop === name) {
+            enumerated = true;
+        }
+    }
+
+    if (enumerated !== enumerable) {
+        attributesCorrect = false;
+    }
+
+
+    var deleted = false;
+
+    try {
+        delete obj[name];
+    } catch (de) {
+    }
+    if (!obj.hasOwnProperty(name)) {
+        deleted = true;
+    }
+    if (deleted !== configurable) {
+        attributesCorrect = false;
+    }
+
+    return attributesCorrect;
+}
--- tests/test262/15.2/15.2.3/15.2.3.6/15.2.3.6-4-95.js
+++ tests/test262/15.2/15.2.3/15.2.3.6/15.2.3.6-4-95.js
@@ -1,31 +1,100 @@
-//   TODO rewrite dataPropertyAttributesAreCorrect
-//   function testcase() 
-//   {
-//     var obj = {
-//       
-//     };
-//     var obj1 = {
-//       length : 10
-//     };
-//     Object.defineProperty(obj, "foo", {
-//       value : obj1,
-//       writable : false,
-//       configurable : false
-//     });
-//     var obj2 = {
-//       length : 20
-//     };
-//     try
-// {      Object.defineProperty(obj, "foo", {
-//         value : obj2
-//       });
-//       return false;}
-//     catch (e)
-// {      return e instanceof TypeError && dataPropertyAttributesAreCorrect(obj, "foo", obj1, false, false, false);}
-// 
-//   }
-//   {
-//     var __result1 = testcase();
-//     var __expect1 = true;
-//   }
-//   
+  function testcase() 
+  {
+    var obj = {
+      
+    };
+    var obj1 = {
+      length : 10
+    };
+    Object.defineProperty(obj, "foo", {
+      value : obj1,
+      writable : false,
+      configurable : false
+    });
+    var obj2 = {
+      length : 20
+    };
+    try
+{      Object.defineProperty(obj, "foo", {
+        value : obj2
+      });
+      return false;}
+    catch (e)
+{      return e instanceof TypeError && dataPropertyAttributesAreCorrect(obj, "foo", obj1, false, false, false);}
+
+  }
+  {
+    var __result1 = testcase();
+    var __expect1 = true;
+  }
+  
+function dataPropertyAttributesAreCorrect(obj,
+                                          name,
+                                          value,
+                                          writable,
+                                          enumerable,
+                                          configurable) {
+    var attributesCorrect = true;
+
+    if (obj[name] !== value) {
+        if (typeof obj[name] === "number" &&
+            isNaN(obj[name]) &&
+            typeof value === "number" &&
+            isNaN(value)) {
+            // keep empty
+        } else {
+            attributesCorrect = false;
+        }
+    }
+
+    try {
+        if (obj[name] === "oldValue") {
+            obj[name] = "newValue";
+        } else {
+            obj[name] = "OldValue";
+        }
+    } catch (we) {
+    }
+
+    var overwrited = false;
+    if (obj[name] !== value) {
+        if (typeof obj[name] === "number" &&
+            isNaN(obj[name]) &&
+            typeof value === "number" &&
+            isNaN(value)) {
+            // keep empty
+        } else {
+            overwrited = true;
+        }
+    }
+    if (overwrited !== writable) {
+        attributesCorrect = false;
+    }
+
+    var enumerated = false;
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop) && prop === name) {
+            enumerated = true;
+        }
+    }
+
+    if (enumerated !== enumerable) {
+        attributesCorrect = false;
+    }
+
+
+    var deleted = false;
+
+    try {
+        delete obj[name];
+    } catch (de) {
+    }
+    if (!obj.hasOwnProperty(name)) {
+        deleted = true;
+    }
+    if (deleted !== configurable) {
+        attributesCorrect = false;
+    }
+
+    return attributesCorrect;
+}
