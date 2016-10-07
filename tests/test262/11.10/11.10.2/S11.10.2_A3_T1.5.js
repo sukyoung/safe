e07f8768357899d787543254a6d5e4e0fe0c2307
Sukyoung Ryu <sukyoung.ryu@gmail.com>
2016-10-07 21:03:12 +0900

    test262: 11.10

---
+++ tests/test262/11.10/11.10.2/S11.10.2_A3_T1.5.js
@@ -0,0 +1,37 @@
+  {
+    var __result1 = ({
+      
+    } ^ (function () 
+    {
+      return 1;
+    })) !== 0;
+    var __expect1 = false;
+  }
+  {
+    var __result2 = ((function () 
+    {
+      return 1;
+    }) ^ {
+      
+    }) !== 0;
+    var __expect2 = false;
+  }
+  {
+    var __result3 = ((function () 
+    {
+      return 1;
+    }) ^ (function () 
+    {
+      return 1;
+    })) !== 0;
+    var __expect3 = false;
+  }
+  {
+    var __result4 = ({
+      
+    } ^ {
+      
+    }) !== 0;
+    var __expect4 = false;
+  }
+  
---
+++ tests/test262/11.10/11.10.3/S11.10.3_A3_T1.1.js
@@ -0,0 +1,17 @@
+  {
+    var __result1 = (true | true) !== 1;
+    var __expect1 = false;
+  }
+  {
+    var __result2 = (new Boolean(true) | true) !== 1;
+    var __expect2 = false;
+  }
+  {
+    var __result3 = (true | new Boolean(true)) !== 1;
+    var __expect3 = false;
+  }
+  {
+    var __result4 = (new Boolean(true) | new Boolean(true)) !== 1;
+    var __expect4 = false;
+  }
+  
