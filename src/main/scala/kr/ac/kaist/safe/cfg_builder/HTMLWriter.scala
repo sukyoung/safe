09b74571ec64d1430c8f9d84890ac369e96c07d2
naldo <jhpjhp0223@gmail.com>
2016-10-09 16:10:41 +0900

    Add instructions for HTMLDebugger

--- src/main/scala/kr/ac/kaist/safe/cfg_builder/HTMLWriter.scala
+++ src/main/scala/kr/ac/kaist/safe/cfg_builder/HTMLWriter.scala
@@ -14,7 +14,7 @@ package kr.ac.kaist.safe.cfg_builder
 import java.io.{ File, FileWriter, BufferedInputStream }
 import scala.collection.immutable.TreeMap
 import kr.ac.kaist.safe.nodes.cfg._
-import kr.ac.kaist.safe.LINE_SEP
+import kr.ac.kaist.safe.{ LINE_SEP, BASE_DIR }
 
 object HTMLWriter {
   val EXC_EDGE = ", width: 2, style: 'dashed', arrow: 'triangle', label:'exc'"
@@ -104,6 +104,20 @@ object HTMLWriter {
     sb.toString
   }
 
+  def addInsts(block: CFGBlock): String = {
+    val sb = new StringBuilder
+    val id = getId(block)
+    val label = getLabel(block)
+    val func = block.func
+    sb.append(s"'$id': [").append(LINE_SEP)
+      .append(s"{ kind: 'Block', id: 'block', value: '$label of $func' },").append(LINE_SEP)
+    block.getInsts.foreach(inst => {
+      sb.append(s"{ kind: 'Instruction', value: '$inst' },").append(LINE_SEP)
+    })
+    sb.append(s"]," + LINE_SEP)
+    sb.toString
+  }
+
   def drawGraph(
     cfg: CFG
   ): String = {
@@ -115,86 +129,40 @@ object HTMLWriter {
       case (func, lst) => func.getAllBlocks ++ lst
     }.reverse
 
+    val base = BASE_DIR + File.separator
+
     val sb = new StringBuilder
-    sb.append("""<html>
-  <head>
-    <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
-    <script src="http://cytoscape.github.io/cytoscape.js/api/cytoscape.js-latest/cytoscape.min.js"></script>
-    <script src="https://cdn.rawgit.com/cpettitt/dagre/v0.7.4/dist/dagre.min.js"></script>
-    <script src="https://cdn.rawgit.com/cytoscape/cytoscape.js-dagre/1.1.2/cytoscape-dagre.js"></script>
-    <style>
-      #cy {
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        left: 0;
-        top: 0;
-        z-index: 999;
-      }
-    </style>
-
-    <script>
-$(function(){
-
-  var cy = window.cy = cytoscape({
-    container: document.getElementById('cy'),
-    layout: {
-      name: 'dagre'
-    },
-
-    style: [
-    {
-      selector: 'node',
-      style: {
-        'shape': 'roundrectangle',
-        'border-width': 'data(border)',
-        'border-color': 'data(color)',
-        'content': 'data(content)',
-        'text-valign': 'center',
-        'text-halign': 'center',
-        'color': 'data(color)',
-        'width': '120',
-        'background-color': 'white',
-      }
-    },
-
-    {
-      selector: 'edge',
-      style: {
-        'width': 'data(width)',
-        'target-arrow-shape': 'data(arrow)',
-        'line-color': 'data(color)',
-        'color': 'data(color)',
-        'target-arrow-color': 'data(color)',
-        'curve-style': 'bezier',
-        'line-style': 'data(style)',
-        'label': 'data(label)',
-        'text-rotation': 'autorotate',
-        'text-background-opacity': 1,
-        'text-background-color': 'white',
-      }
-    }
-    ],
-
-    elements: {
-      nodes: [
+    sb.append(s"""<!DOCTYPE HTML>
+<html>
+    <head>
+        <script src="${base}lib/debugger/jquery-2.0.3.min.js"></script>
+        <script src="${base}lib/debugger/cytoscape.min.js"></script>
+        <script src="${base}lib/debugger/dagre.min.js"></script>
+        <script src="${base}lib/debugger/cytoscape-dagre.js"></script>
+        <script src="${base}lib/debugger/webix.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="${base}lib/debugger/css/webix.css" type="text/css">
+        <link rel="stylesheet" href="${base}lib/debugger/css/cy.css" type="text/css">
+        <script>
+var safe_DB = {
+  nodes: [
 """)
     blocks.foreach(block => sb.append(drawBlock(block)))
-    sb.append("""
-      ],
-      edges: [
+    sb.append("""  ],
+  edges: [
 """)
     blocks.foreach(block => sb.append(drawEdge(block)))
-    sb.append("""      ]
-    },
-  });
-
-});
-    </script>
-  </head>
-  <body>
-    <div id="cy"></div>
-  </body>
+    sb.append("""  ],
+  insts: {
+""")
+    blocks.foreach(block => sb.append(addInsts(block)))
+    sb.append(s"""  },
+};
+        </script>
+        <script src="${base}lib/debugger/core.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div id='cy'></div>
+    </body>
 </html>""")
     sb.toString
   }
