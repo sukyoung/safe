1b0790a3766cf71f59805895b74468157e1986ab
naldo <jhpjhp0223@gmail.com>
2017-02-14 15:52:42 +0900

    fix error: test262 test S15.2.2.1_A2_T1.js

--- tests/test262/15.2/15.2.2/S15.2.2.1_A2_T1.js
+++ tests/test262/15.2/15.2.2/S15.2.2.1_A2_T1.js
@@ -2,8 +2,8 @@ var obj = {prop:1};
 
 var n_obj = new Object(obj);
 
-var __result1 = obj;
-var __expect1 = n_obj;
+var __result1 = n_obj;
+var __expect1 = obj;
 
 var __result2 = n_obj['prop'];
 var __expect2 = 1;
