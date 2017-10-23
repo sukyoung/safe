fff2824b39057af75d11c2b5b5e5261ca9b7e4d0
Max Schlueter <mschlueter@uni-potsdam.de>
2017-10-23 15:04:04 +0200

    Rewrote tests and removed '$ERROR' lines. [[DefaultValue]] should throw TypeError exceptions in some cases.

--- tests/test262/8.12/8.12.8/S8.12.8_A2.js
+++ tests/test262/8.12/8.12.8/S8.12.8_A2.js
@@ -1,19 +1,23 @@
-  try
-{    var __obj = {
-      toString : (function () 
-      {
-        return new Object();
-      }),
-      valueOf : (function () 
-      {
-        return 1;
-      })
-    };
+var __result1 = false;
+var __result2 = true;
+try {
+  var __obj = {
+    toString : (function ()
     {
-      var __result1 = String(__obj) !== "1";
-      var __expect1 = false;
-    }}
-  catch (e)
-{    $ERROR('#1.2: var __obj = {toString: function() {return new Object();}, valueOf: function() {return 1;}}; String(__obj) === "1". Actual: ' + (e));}
+      return new Object();
+    }),
+    valueOf : (function ()
+    {
+      return 1;
+    })
+  };
+  if (String(__obj) !== "1") {
+    __result1 = true;
+  }
+}
+catch (e) {
+  __result2 = false;
+}
 
-  
+var __expect1 = true;
+var __expect2 = true;
--- tests/test262/8.12/8.12.8/S8.12.8_A3.js
+++ tests/test262/8.12/8.12.8/S8.12.8_A3.js
@@ -1,19 +1,23 @@
-  try
-{    var __obj = {
-      toString : (function () 
-      {
-        return "1";
-      }),
-      valueOf : (function () 
-      {
-        return new Object();
-      })
-    };
+var __result1 = true;
+var __result2 = true;
+try {
+  var __obj = {
+    toString : (function ()
     {
-      var __result1 = Number(__obj) !== 1;
-      var __expect1 = false;
-    }}
-  catch (e)
-{    $ERROR('#1.2: var __obj = {toNumber: function() {return "1"}, valueOf: function() {return new Object();}}; Number(__obj) === 1. Actual: ' + (e));}
+      return "1";
+    }),
+    valueOf : (function ()
+    {
+      return new Object();
+    })
+  };
+  if (Number(__obj) !== 1) {
+    __result1 = false;
+  }
+}
+catch (e) {
+  __result2 = false;
+}
 
-  
+var __expect1 = true;
+var __expect2 = true;
