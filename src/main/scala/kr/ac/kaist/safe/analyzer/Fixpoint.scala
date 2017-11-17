79af9cc56791cb14f00860d04e76cf5669704002
Changhee Park <changheepark@Changheeui-MacBook-Pro.local>
2017-11-17 14:14:35 +0830

    [NodeJS] Minor edit for testing.

--- src/main/scala/kr/ac/kaist/safe/analyzer/Fixpoint.scala
+++ src/main/scala/kr/ac/kaist/safe/analyzer/Fixpoint.scala
@@ -28,7 +28,7 @@ class Fixpoint(
       consoleOpt.fold() { _.runFixpoint }
       val cp = worklist.pop
       val st = semantics.getState(cp)
-      println("Interation  " + iters + ": " + cp)
+      // println("Interation  " + iters + ": " + cp)
       val (nextSt, nextExcSt) = semantics.C(cp, st)
       propagateNormal(cp, nextSt)
       propagateException(cp, nextExcSt)
