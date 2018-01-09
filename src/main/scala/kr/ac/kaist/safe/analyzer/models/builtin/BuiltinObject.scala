8955729395d95125e4eab6ccd0e0c0286a105c5c
jhnaldo <jhpjhp0223@gmail.com>
2018-01-09 12:56:05 +0900

    Fix error in Object.defineProperties

--- src/main/scala/kr/ac/kaist/safe/analyzer/models/builtin/BuiltinObject.scala
+++ src/main/scala/kr/ac/kaist/safe/analyzer/models/builtin/BuiltinObject.scala
@@ -430,7 +430,7 @@ object BuiltinObjectHelper {
     val propsV = Helper.propLoad(args, Set(AbsStr("1")), h)
 
     val (retSt, excSet) = defProps(objV, propsV, st)
-    val excSt = st.raiseException(excSet)
+    val excSt = st.raiseException(excSet) ⊔ retSt.raiseException(excSet)
 
     (retSt, excSt, objV.locset)
   }
