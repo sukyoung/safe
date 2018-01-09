8602d7573923e38255ee29d117787397a3995636
jhnaldo <jhpjhp0223@gmail.com>
2018-01-09 14:42:29 +0900

    Handled exception case in String.prototype.trim

--- src/main/scala/kr/ac/kaist/safe/analyzer/models/builtin/BuiltinString.scala
+++ src/main/scala/kr/ac/kaist/safe/analyzer/models/builtin/BuiltinString.scala
@@ -566,11 +566,13 @@ object BuiltinStringProto extends ObjModel(
         // 1. Call CheckObjectCoercible passing the this value as its argument.
         // 2. Let S be the result of calling ToString, giving it the this value as its argument.
         val thisV = st.context.thisBinding
+        val excSet = TypeConversionHelper.CheckObjectCoercible(thisV)
         // 3. Let T be a String value that is a copy of S with both leading and trailing white space
         //   removed. The definition of white space is the union of WhiteSpace and LineTerminator.
         // 4. Return T.
         val s = TypeConversionHelper.ToString(thisV, h).trim
-        (st, AbsState.Bot, AbsValue(s))
+        val excSt = st.raiseException(excSet)
+        (st, excSt, AbsValue(s))
       })
     ), T, F, T)
   )
