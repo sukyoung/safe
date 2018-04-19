b9ce77a92d76d46349c9bdc03e33647700155c8f
naldo <jhpjhp0223@gmail.com>
2018-04-19 15:48:34 +0300

    [Copyright] Updated Copyright 2018

--- src/main/scala/kr/ac/kaist/safe/errors/error/CompileError.scala
+++ src/main/scala/kr/ac/kaist/safe/errors/error/CompileError.scala
@@ -1,6 +1,6 @@
 /**
  * *****************************************************************************
- * Copyright (c) 2016-2017, KAIST.
+ * Copyright (c) 2016-2018, KAIST.
  * All rights reserved.
  *
  * Use is subject to license terms.
--- src/main/scala/kr/ac/kaist/safe/json/NodeProtocol.scala
+++ src/main/scala/kr/ac/kaist/safe/json/NodeProtocol.scala
@@ -1,6 +1,6 @@
 /**
  * *****************************************************************************
- * Copyright (c) 2017, KAIST.
+ * Copyright (c) 2018, KAIST.
  * All rights reserved.
  *
  * Use is subject to license terms.
