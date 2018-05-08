dc904e4723b133a0b1478c21be80fb78a9c18944
jhnaldo <jhpjhp0223@gmail.com>
2018-05-08 15:31:46 +0900

    Fix error in checking copyrights

--- tests/bin/test_gen.sh
+++ tests/bin/test_gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ################################################################################
-#    Copyright (c) 2016-2017, KAIST.
+#    Copyright (c) 2016-2018, KAIST.
 #    All rights reserved.
 #
 #    Use is subject to license terms.
