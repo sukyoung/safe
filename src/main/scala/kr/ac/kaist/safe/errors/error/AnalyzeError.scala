0c575340b0825cfc68cfb4d0ff585385505f68e1
Jaemin Hong <hjm0901@gmail.com>
2019-10-11 11:36:20 +0900

    resolve missing string interpolation

--- src/main/scala/kr/ac/kaist/safe/errors/error/AnalyzeError.scala
+++ src/main/scala/kr/ac/kaist/safe/errors/error/AnalyzeError.scala
@@ -94,5 +94,5 @@ case class NoFuncIdError(fid: Int) extends AnalyzeError({
 case object IllFormedBlockStr extends AnalyzeError("")
 
 case class NotYetDefined(name: String) extends AnalyzeError(
-  "[NotYetDefined] $name"
+  s"[NotYetDefined] $name"
 )
