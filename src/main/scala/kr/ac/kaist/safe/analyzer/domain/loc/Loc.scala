e1df09f49ae9a2d1fa4d1cfc8a2a3db5862b7081
PLRG iMac Late 2015 <joonyoungpark@iMac-3.local>
2020-10-27 15:08:06 +0900

    Added first draft of introduction.

--- src/main/scala/kr/ac/kaist/safe/analyzer/domain/loc/Loc.scala
+++ src/main/scala/kr/ac/kaist/safe/analyzer/domain/loc/Loc.scala
@@ -55,7 +55,7 @@ object Loc {
 trait LocParser extends TracePartitionParser {
   // allocation site abstraction
   lazy val userASite = "#" ~> nat ^^ { id => UserAllocSite(id) }
-  lazy val predName = "[0-9a-zA-Z-.<>\\[\\][_]]+".r
+  lazy val predName = "[0-9a-zA-Z-.<>\\[\\]_]+".r
   lazy val predASite = "#" ~> predName ^^ { name => PredAllocSite(name) }
   lazy val allocSite = userASite | predASite
 
