5ffdd4c2e41e74f11d6a16f0ed603c6ec70df544
DJ <f52985@gmail.com>
2020-11-02 14:46:03 +0900

    Change tab into space in setting.js

--- tests/benchmarks/lodash4/setting.js
+++ tests/benchmarks/lodash4/setting.js
@@ -122,153 +122,153 @@ function objectType(obj) {
   }
 }
 var equiv = (function () {
-	  var pairs = [];
-	  var getProto = Object.getPrototypeOf || function (obj) {
-	    return obj.__proto__;
-	  };
-	  function useStrictEquality(a, b) {
-	    if (_typeof(a) === "object") a = a.valueOf();
-	    if (_typeof(b) === "object") b = b.valueOf();
-	    return a === b;
-	  }
-	  function compareConstructors(a, b) {
-	    var protoA = getProto(a);
-	    var protoB = getProto(b);
-	    if (a.constructor === b.constructor) return true;
-	    if (protoA && protoA.constructor === null) protoA = null;
-	    if (protoB && protoB.constructor === null) protoB = null;
-	    if (protoA === null && protoB === Object.prototype || protoB === null && protoA === Object.prototype) return true;
-	    return false;
-	  }
-	  function getRegExpFlags(regexp) {
-	    return "flags" in regexp ? regexp.flags : regexp.toString().match(/[gimuy]*$/)[0];
-	  }
-	  function isContainer(val) {
-	    return ["object", "array", "map", "set"].indexOf(objectType(val)) !== -1;
-	  }
-	  function breadthFirstCompareChild(a, b) {
-	    if (a === b) return true;
-	    if (!isContainer(a)) return typeEquiv(a, b);
-	    if (pairs.every(function (pair) {
-	      return pair.a !== a || pair.b !== b;
-	    })) {
-	      pairs.push({
-	        a: a,
-	        b: b
-	      });
-	    }
-	    return true;
-	  }
-	  var callbacks = {
-	    "string": useStrictEquality,
-	    "boolean": useStrictEquality,
-	    "number": useStrictEquality,
-	    "null": useStrictEquality,
-	    "undefined": useStrictEquality,
-	    "symbol": useStrictEquality,
-	    "date": useStrictEquality,
-	    "nan": function nan() {
-	      return true;
-	    },
-	    "regexp": function regexp(a, b) {
-	      return a.source === b.source &&
-	      getRegExpFlags(a) === getRegExpFlags(b);
-	    },
-	    "function": function _function() {
-	      return false;
-	    },
-	    "array": function array(a, b) {
-	      var i, len;
-	      len = a.length;
-	      if (len !== b.length) {
-	        return false;
-	      }
-	      for (i = 0; i < len; i++) {
-	        if (!breadthFirstCompareChild(a[i], b[i])) {
-	          return false;
-	        }
-	      }
+    var pairs = [];
+    var getProto = Object.getPrototypeOf || function (obj) {
+      return obj.__proto__;
+    };
+    function useStrictEquality(a, b) {
+      if (_typeof(a) === "object") a = a.valueOf();
+      if (_typeof(b) === "object") b = b.valueOf();
+      return a === b;
+    }
+    function compareConstructors(a, b) {
+      var protoA = getProto(a);
+      var protoB = getProto(b);
+      if (a.constructor === b.constructor) return true;
+      if (protoA && protoA.constructor === null) protoA = null;
+      if (protoB && protoB.constructor === null) protoB = null;
+      if (protoA === null && protoB === Object.prototype || protoB === null && protoA === Object.prototype) return true;
+      return false;
+    }
+    function getRegExpFlags(regexp) {
+      return "flags" in regexp ? regexp.flags : regexp.toString().match(/[gimuy]*$/)[0];
+    }
+    function isContainer(val) {
+      return ["object", "array", "map", "set"].indexOf(objectType(val)) !== -1;
+    }
+    function breadthFirstCompareChild(a, b) {
+      if (a === b) return true;
+      if (!isContainer(a)) return typeEquiv(a, b);
+      if (pairs.every(function (pair) {
+        return pair.a !== a || pair.b !== b;
+      })) {
+        pairs.push({
+          a: a,
+          b: b
+        });
+      }
+      return true;
+    }
+    var callbacks = {
+      "string": useStrictEquality,
+      "boolean": useStrictEquality,
+      "number": useStrictEquality,
+      "null": useStrictEquality,
+      "undefined": useStrictEquality,
+      "symbol": useStrictEquality,
+      "date": useStrictEquality,
+      "nan": function nan() {
+        return true;
+      },
+      "regexp": function regexp(a, b) {
+        return a.source === b.source &&
+        getRegExpFlags(a) === getRegExpFlags(b);
+      },
+      "function": function _function() {
+        return false;
+      },
+      "array": function array(a, b) {
+        var i, len;
+        len = a.length;
+        if (len !== b.length) {
+          return false;
+        }
+        for (i = 0; i < len; i++) {
+          if (!breadthFirstCompareChild(a[i], b[i])) {
+            return false;
+          }
+        }
 
-	      return true;
-	    },
-	    "set": function set(a, b) {
-	      var innerEq,
-	          outerEq = true;
-	      if (a.size !== b.size) {
-	        return false;
-	      }
-	      a.forEach(function (aVal) {
-	        if (!outerEq) return;
-	        innerEq = false;
-	        b.forEach(function (bVal) {
-	          var parentPairs;
-	          if (innerEq) return;
-	          parentPairs = pairs;
-	          if (innerEquiv(bVal, aVal)) innerEq = true;
-	          pairs = parentPairs;
-	        });
-	        if (!innerEq) {
-	          outerEq = false;
-	        }
-	      });
-	      return outerEq;
-	    },
-	    "map": function map(a, b) {
-	      var innerEq,
-	          outerEq = true;
-	      if (a.size !== b.size) return false;
-	      a.forEach(function (aVal, aKey) {
-	        if (!outerEq) return;
-	        innerEq = false;
-	        b.forEach(function (bVal, bKey) {
-	          var parentPairs;
-	          if (innerEq) return;
-	          parentPairs = pairs;
-	          if (innerEquiv([bVal, bKey], [aVal, aKey])) innerEq = true;
-	          pairs = parentPairs;
-	        });
-	        if (!innerEq) outerEq = false;
-	      });
-	      return outerEq;
-	    },
-	    "object": function object(a, b) {
-	      var i,
-	          aProperties = [],
-	          bProperties = [];
-	      if (compareConstructors(a, b) === false) return false;
-	      for (i in a) {
-	        aProperties.push(i);
-	        if (a.constructor !== Object && typeof a.constructor !== "undefined" && typeof a[i] === "function" && typeof b[i] === "function" && a[i].toString() === b[i].toString()) continue;
-	        if (!breadthFirstCompareChild(a[i], b[i])) return false;
-	      }
-	      for (i in b) {
-	        bProperties.push(i);
-	      }
-	      return typeEquiv(aProperties.sort(), bProperties.sort());
-	    }
-	  };
-	  function typeEquiv(a, b) {
-	    var type = objectType(a);
-	    return objectType(b) === type && callbacks[type](a, b);
-	  }
-	  function innerEquiv(a, b) {
-	    var i, pair;
-	    if (arguments.length < 2) return true;
-	    pairs = [{
-	      a: a,
-	      b: b
-	    }];
-	    for (i = 0; i < pairs.length; i++) {
-	      pair = pairs[i];
-	      if (pair.a !== pair.b && !typeEquiv(pair.a, pair.b)) {
-	        return false;
-	      }
-	    }
-	    return arguments.length === 2 || innerEquiv.apply(this, [].slice.call(arguments, 1));
-	  }
-	  return function () {
-	    var result = innerEquiv.apply(void 0, arguments);
-	    pairs.length = 0;
-	    return result;
-	  };
-	})();
+        return true;
+      },
+      "set": function set(a, b) {
+        var innerEq,
+            outerEq = true;
+        if (a.size !== b.size) {
+          return false;
+        }
+        a.forEach(function (aVal) {
+          if (!outerEq) return;
+          innerEq = false;
+          b.forEach(function (bVal) {
+            var parentPairs;
+            if (innerEq) return;
+            parentPairs = pairs;
+            if (innerEquiv(bVal, aVal)) innerEq = true;
+            pairs = parentPairs;
+          });
+          if (!innerEq) {
+            outerEq = false;
+          }
+        });
+        return outerEq;
+      },
+      "map": function map(a, b) {
+        var innerEq,
+            outerEq = true;
+        if (a.size !== b.size) return false;
+        a.forEach(function (aVal, aKey) {
+          if (!outerEq) return;
+          innerEq = false;
+          b.forEach(function (bVal, bKey) {
+            var parentPairs;
+            if (innerEq) return;
+            parentPairs = pairs;
+            if (innerEquiv([bVal, bKey], [aVal, aKey])) innerEq = true;
+            pairs = parentPairs;
+          });
+          if (!innerEq) outerEq = false;
+        });
+        return outerEq;
+      },
+      "object": function object(a, b) {
+        var i,
+            aProperties = [],
+            bProperties = [];
+        if (compareConstructors(a, b) === false) return false;
+        for (i in a) {
+          aProperties.push(i);
+          if (a.constructor !== Object && typeof a.constructor !== "undefined" && typeof a[i] === "function" && typeof b[i] === "function" && a[i].toString() === b[i].toString()) continue;
+          if (!breadthFirstCompareChild(a[i], b[i])) return false;
+        }
+        for (i in b) {
+          bProperties.push(i);
+        }
+        return typeEquiv(aProperties.sort(), bProperties.sort());
+      }
+    };
+    function typeEquiv(a, b) {
+      var type = objectType(a);
+      return objectType(b) === type && callbacks[type](a, b);
+    }
+    function innerEquiv(a, b) {
+      var i, pair;
+      if (arguments.length < 2) return true;
+      pairs = [{
+        a: a,
+        b: b
+      }];
+      for (i = 0; i < pairs.length; i++) {
+        pair = pairs[i];
+        if (pair.a !== pair.b && !typeEquiv(pair.a, pair.b)) {
+          return false;
+        }
+      }
+      return arguments.length === 2 || innerEquiv.apply(this, [].slice.call(arguments, 1));
+    }
+    return function () {
+      var result = innerEquiv.apply(void 0, arguments);
+      pairs.length = 0;
+      return result;
+    };
+  })();
