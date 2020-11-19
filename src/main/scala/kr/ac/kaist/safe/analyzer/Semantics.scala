b8995d44528bb822098ca74c2d5590b29fa81c6a
jhnaldo <jhpjhp0223@gmail.com>
2020-11-19 10:42:44 +0900

    Using stderr in debug message

--- src/main/scala/kr/ac/kaist/safe/analyzer/Semantics.scala
+++ src/main/scala/kr/ac/kaist/safe/analyzer/Semantics.scala
@@ -291,7 +291,7 @@ case class Semantics(
 
             dsCount += 1
 
-            println(s"[DS] [${func.id}] ${func.simpleName} @ ${func.span}")
+            System.err.println(s"[DS] [${func.id}] ${func.simpleName} @ ${func.span}")
 
             val newTP = cp.tracePartition
             val exitCP = ControlPoint(func.exit, newTP)
